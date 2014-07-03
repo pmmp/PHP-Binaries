147dacdf724ea147eb80dc555137d23259f9c8d0
Shoghi Cervantes <shoghicp@gmail.com>
2014-07-04 01:59:43 +0200

    Fixed pthreads test

--- tests/generic/pthreads.sh
+++ tests/generic/pthreads.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OUTPUT=$("$PHP_BINARIES" -r 'class MyThread extends \\Thread{ public function run(){ echo 1; }} $t = new MyThread; $t->start(); $t->join();')
+OUTPUT=$("$PHP_BINARIES" -r 'class MyThread extends Thread{ public function run(){ echo 1; }} $t = new MyThread; $t->start(); $t->join(); echo "1";')
 
 if [ "$OUTPUT" != "1" ]; then
 	exit 1
