69e7f3289f22e889d2edd93dd7968848a4a1c153
Dylan K. Taylor <odigiman@gmail.com>
2017-05-30 18:22:44 +0100

    Do not compile leveldb unless explicitly instructed to do so

--- compile.sh
+++ compile.sh
@@ -157,9 +157,7 @@ while getopts "::t:oj:srcdlxzff:u" OPTION; do
 done
 
 
-if [ $(gcc -dumpversion | sed -e 's/\.\([0-9][0-9]\)/\1/g' -e 's/\.\([0-9]\)/0\1/g' -e 's/^[0-9]\{3,4\}$/&00/') -gt 40800 ]; then
-	COMPILE_LEVELDB="yes"
-else
+if ! [ $(gcc -dumpversion | sed -e 's/\.\([0-9][0-9]\)/\1/g' -e 's/\.\([0-9]\)/0\1/g' -e 's/^[0-9]\{3,4\}$/&00/') -gt 40800 ]; then
 	COMPILE_LEVELDB="no"
 fi
 
