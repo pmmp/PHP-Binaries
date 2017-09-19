2485e87a86560c8eaf4c80778357f71bd07052fa
Dylan K. Taylor <odigiman@gmail.com>
2017-09-19 13:46:14 +0100

    Remove obsolete Jenkins scripts [appveyor skip]

--- jenkins_startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-export BRANCH
-wget --no-check-certificate -q -O - https://github.com/PocketMine/php-build-scripts/raw/$BRANCH/jenkins.sh > "$WORKSPACE/jenkins.sh"
-chmod +x "$WORKSPACE/jenkins.sh"
-$WORKSPACE/jenkins.sh
