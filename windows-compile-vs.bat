2ebb473844bcea069c3c8e127009f07a92e5cd94
Dylan K. Taylor <odigiman@gmail.com>
2017-10-27 12:28:46 +0100

    update windows PHP SDK version

--- windows-compile-vs.bat
+++ windows-compile-vs.bat
@@ -2,7 +2,7 @@
 
 set PHP_MAJOR_VER=7.2
 set PHP_VER=%PHP_MAJOR_VER%.0RC5
-set PHP_SDK_VER=2.0.11
+set PHP_SDK_VER=2.0.12
 set PATH=C:\Program Files\7-Zip;C:\Program Files (x86)\GnuWin32\bin;%PATH%
 set VC_VER=vc15
 set ARCH=x64
