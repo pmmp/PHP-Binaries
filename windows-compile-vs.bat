3dcd94c0c30423620dc0332173e3946658b4026c
Dylan K. Taylor <odigiman@gmail.com>
2019-03-07 19:22:09 +0000

    Update some versions

--- windows-compile-vs.bat
+++ windows-compile-vs.bat
@@ -3,7 +3,7 @@
 REM For future users: This file MUST have CRLF line endings. If it doesn't, lots of inexplicable undesirable strange behaviour will result.
 REM Also: Don't modify this version with sed, or it will screw up your line endings.
 set PHP_MAJOR_VER=7.2
-set PHP_VER=%PHP_MAJOR_VER%.15
+set PHP_VER=%PHP_MAJOR_VER%.16
 set PHP_IS_BETA="no"
 set PHP_SDK_VER=2.1.9
 set PATH=C:\Program Files\7-Zip;C:\Program Files (x86)\GnuWin32\bin;%PATH%
@@ -19,7 +19,7 @@ set PHP_PTHREADS_VER=17c9966bac59211da0705166fc0ecb5ecbc96a0d
 set PHP_YAML_VER=2.0.4
 set PHP_POCKETMINE_CHUNKUTILS_VER=master
 set PHP_IGBINARY_VER=2.0.8
-REM this is 1.2.7 but tags with a "v" prefix are a pain in the ass
+REM this is 1.2.8 but tags with a "v" prefix are a pain in the ass
 set PHP_DS_VER=4bb4be24ce9835ca81be2e48f0104683e41bce12
 set PHP_LEVELDB_VER=9bcae79f71b81a5c3ea6f67e45ae9ae9fb2775a5
 set PHP_CRYPTO_VER=5f26ac91b0ba96742cc6284cd00f8db69c3788b2
