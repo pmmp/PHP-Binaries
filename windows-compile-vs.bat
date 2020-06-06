df5f337c38d10d8cbfa9bcfedf75fb13d92db399
Dylan K. Taylor <odigiman@gmail.com>
2020-06-06 21:17:32 +0100

    windows: rc counts as beta

--- windows-compile-vs.bat
+++ windows-compile-vs.bat
@@ -4,7 +4,7 @@ REM For future users: This file MUST have CRLF line endings. If it doesn't, lots
 REM Also: Don't modify this version with sed, or it will screw up your line endings.
 set PHP_MAJOR_VER=7.4
 set PHP_VER=%PHP_MAJOR_VER%.7RC1
-set PHP_IS_BETA="no"
+set PHP_IS_BETA="yes"
 set PHP_SDK_VER=2.2.0
 set PATH=C:\Program Files\7-Zip;C:\Program Files (x86)\GnuWin32\bin;%PATH%
 set VC_VER=vc15
