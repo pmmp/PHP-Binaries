33b59b846af14b4ffbbe000c0c64befa336f409e
Matt Clare <matt@mattclare.ca>
2022-04-27 23:29:28 -0400

    Update installer.sh
    switch to double quotes
    
    Co-authored-by: Dylan T. <odigiman@gmail.com>

--- installer.sh
+++ installer.sh
@@ -303,7 +303,7 @@ else
 		set -e
 		echo "[3/3] No prebuilt PHP found, compiling PHP automatically. This might take a while."
 		echo
-		logical_cpu_count=$([ $(uname) = 'Darwin' ] && sysctl -n hw.logicalcpu_max || nproc --all) #Get available CPUs to pass to compile.sh and speed up compile
+		logical_cpu_count=$([ "$(uname -s)" == "Darwin" ] && sysctl -n hw.logicalcpu_max || nproc --all) #Get available CPUs to pass to compile.sh and speed up compile
 		if [ $logical_cpu_count -gt 0 ];
 		then
 			echo "Starting $logical_cpu_count thread compile"
