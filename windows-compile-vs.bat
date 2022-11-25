41699d75ec96730d897d7a39a0fb2cec9e2db76f
Dylan K. Taylor <dktapps@pmmp.io>
2022-11-25 21:07:27 +0000

    Updated PHP and dependency versions

--- windows-compile-vs.bat
+++ windows-compile-vs.bat
@@ -3,7 +3,7 @@
 REM For future users: This file MUST have CRLF line endings. If it doesn't, lots of inexplicable undesirable strange behaviour will result.
 REM Also: Don't modify this version with sed, or it will screw up your line endings.
 set PHP_MAJOR_VER=8.0
-set PHP_VER=%PHP_MAJOR_VER%.24
+set PHP_VER=%PHP_MAJOR_VER%.25
 set PHP_GIT_REV=php-%PHP_VER%
 set PHP_DISPLAY_VER=%PHP_VER%
 set PHP_SDK_VER=2.2.0
@@ -26,14 +26,14 @@ set LIBDEFLATE_VER=0d1779a071bcc636e5156ddb7538434da7acad22
 set PHP_PTHREADS_VER=4.1.4
 set PHP_YAML_VER=2.2.2
 set PHP_CHUNKUTILS2_VER=0.3.3
-set PHP_IGBINARY_VER=3.2.7
+set PHP_IGBINARY_VER=3.2.12
 set PHP_LEVELDB_VER=317fdcd8415e1566fc2835ce2bdb8e19b890f9f3
 set PHP_CRYPTO_VER=0.3.2
 set PHP_RECURSIONGUARD_VER=0.1.0
 set PHP_MORTON_VER=0.1.2
 set PHP_LIBDEFLATE_VER=0.1.0
 set PHP_XXHASH_VER=0.1.1
-set PHP_XDEBUG_VER=3.1.5
+set PHP_XDEBUG_VER=3.1.6
 
 set script_path=%~dp0
 set log_file=%script_path%compile.log
